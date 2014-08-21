124dcda01e5bb80f43fd1db739efa0644db763e2
Doug Barth <doug@pagerduty.com>
2014-08-21 16:11:14 -0700

    Checking DB health_check schema

--- app.rb
+++ app.rb
@@ -1,16 +1,40 @@
 require 'sinatra'
+require 'mysql2'
+require 'json'
 
 configure do
   set :wsrep_state_dir, '/etc/mysql/wsrep'
+
+  set(:hostname) { `hostname` }
 end
 
 configure :development do
   set :wsrep_state_dir, 'spec/data/3_node_cluster_synced'
+
+  set :hostname, 'dev.local'
+end
+
+configure :test do
+  set :hostname, 'dev.local'
 end
 
 get '/' do
+  state_info = {}
+
   state = File.read(File.join(settings.wsrep_state_dir, 'status')).strip
   size = File.read(File.join(settings.wsrep_state_dir, 'size')).strip.to_i
+
+  state_info['wsrep_local_status'] = state
+  state_info['cluster_size'] = size
+
+  client = Mysql2::Client.new(
+    host: 'localhost',
+    username: 'root', 
+    database: 'health_check')
+
+  results = client.query("SELECT available FROM state WHERE host_name = '#{settings.hostname}'")
+  health_check_state = results.first['available']
+  state_info['health_check.state'] = health_check_state
 
   up = case state
        when 'Synced'
@@ -21,9 +45,13 @@ get '/' do
          false
        end
 
+  up &&= health_check_state == 1
+
+  state_info['available'] = up
+
   if up
-    [200, "OK"]
+    [200, JSON.generate(state_info)]
   else
-    [503, "Not OK"]
+    [503, JSON.generate(state_info)]
   end
 end
