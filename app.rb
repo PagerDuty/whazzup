a6b3b64f9c0b3c80e0c50866b3759a668a859127
Doug Barth <doug@pagerduty.com>
2014-09-10 13:56:59 -0700

    Extracting a Galera health checker object
    This is on the road to having a singleton that manages rate limiting
    checks against the service.

--- app.rb
+++ app.rb
@@ -19,43 +19,86 @@ configure :test do
 end
 
 get '/' do
-  state_info = {}
+  checker = GaleraHealthChecker.new(
+    wsrep_state_dir: settings.wsrep_state_dir,
+    connection_settings: {
+      host: 'localhost',
+      username: 'root', 
+      database: 'health_check'
+    },
+    hostname: settings.hostname
+  )
 
-  begin
-    state = File.read(File.join(settings.wsrep_state_dir, 'status')).strip
-    size = File.read(File.join(settings.wsrep_state_dir, 'size')).strip.to_i
-  rescue => e
-    # TODO log the exception here
-  end
-
-  state_info['wsrep_local_status'] = state
-  state_info['cluster_size'] = size
-
-  begin
-    results = db_client.query("SELECT available FROM state WHERE host_name = '#{settings.hostname}'")
-    health_check_state = results.first ? results.first['available'] : 0
-  rescue => e
-    health_check_state = 0
-  end
-  state_info['health_check.state'] = health_check_state
-
-  up = case state
-       when 'Synced'
-         true
-       when 'Donor'
-         size == 2
-       else
-         false
-       end
-
-  up &&= health_check_state == 1
-
-  state_info['available'] = up
-
-  if up
-    [200, JSON.generate(state_info)]
+  if checker.check
+    [200, JSON.generate(checker.check_details)]
   else
-    [503, JSON.generate(state_info)]
+    [503, JSON.generate(checker.check_details)]
+  end
+end
+
+class GaleraHealthChecker
+  attr_accessor :wsrep_state_dir
+  attr_accessor :connection_settings
+  attr_accessor :hostname
+
+  # Returns an object that should be passed back to the client to give details
+  # on the state of the service
+  attr_reader :check_details
+
+  def initialize(settings = {})
+    self.wsrep_state_dir = settings[:wsrep_state_dir]
+    self.connection_settings = settings[:connection_settings]
+    self.hostname = settings[:hostname]
+  end
+
+  # Returns true if the service is up
+  def check
+    check_details = {}
+
+    up = check_wsrep_state(check_details)
+    up &&= check_state_table(check_details)
+
+    check_details['available'] = up
+
+    @check_details = check_details
+
+    return up
+  end
+
+  def check_wsrep_state(check_details)
+    begin
+      state = File.read(File.join(wsrep_state_dir, 'status')).strip
+      size = File.read(File.join(wsrep_state_dir, 'size')).strip.to_i
+    rescue => e
+      # TODO log the exception here
+    end
+
+    check_details['wsrep_local_status'] = state
+    check_details['cluster_size'] = size
+
+    up = case state
+         when 'Synced'
+           true
+         when 'Donor'
+           size == 2
+         else
+           false
+         end
+
+    return up
+  end
+
+  def check_state_table(check_details)
+    begin
+      results = db_client.query("SELECT available FROM state WHERE host_name = '#{hostname}'")
+      health_check_state = results.first ? results.first['available'] : 0
+    rescue => e
+      health_check_state = 0
+    end
+
+    check_details['health_check.state'] = health_check_state
+
+    return health_check_state == 1
   end
 end
 
