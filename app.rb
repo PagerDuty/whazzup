70a12fa5fdfbabfd9011ea5a3eaa7ad0541f7a78
Doug Barth <doug@pagerduty.com>
2014-09-10 14:08:08 -0700

    Moving GaleraHealthChecker to its own file

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'mysql2'
 require 'json'
 
+require_relative 'lib/galera_health_checker'
+
 configure do
   set :wsrep_state_dir, '/etc/mysql/wsrep'
 
@@ -40,74 +42,3 @@ get '/' do
     [503, JSON.generate(checker.check_details)]
   end
 end
-
-class GaleraHealthChecker
-  attr_accessor :wsrep_state_dir
-  attr_accessor :connection_settings
-  attr_accessor :hostname
-
-  # Returns an object that should be passed back to the client to give details
-  # on the state of the service
-  attr_reader :check_details
-
-  def initialize(settings = {})
-    self.wsrep_state_dir = settings[:wsrep_state_dir]
-    self.connection_settings = settings[:connection_settings]
-    self.hostname = settings[:hostname]
-  end
-
-  # Returns true if the service is up
-  def check
-    check_details = {}
-
-    up = check_wsrep_state(check_details)
-    up &&= check_state_table(check_details)
-
-    check_details['available'] = up
-
-    @check_details = check_details
-
-    return up
-  end
-
-  def check_wsrep_state(check_details)
-    begin
-      state = File.read(File.join(wsrep_state_dir, 'status')).strip
-      size = File.read(File.join(wsrep_state_dir, 'size')).strip.to_i
-    rescue => e
-      # TODO log the exception here
-    end
-
-    check_details['wsrep_local_status'] = state
-    check_details['cluster_size'] = size
-
-    up = case state
-         when 'Synced'
-           true
-         when 'Donor'
-           size == 2
-         else
-           false
-         end
-
-    return up
-  end
-
-  def check_state_table(check_details)
-    begin
-      results = db_client.query("SELECT available FROM state WHERE host_name = '#{hostname}'")
-      health_check_state = results.first ? results.first['available'] : 0
-    rescue => e
-      health_check_state = 0
-    end
-
-    check_details['health_check.state'] = health_check_state
-
-    return health_check_state == 1
-  end
-
-  def db_client
-    @db_client ||= Mysql2::Client.new(connection_settings)
-  end
-end
-
